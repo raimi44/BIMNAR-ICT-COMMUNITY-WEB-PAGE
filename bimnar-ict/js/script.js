3e043c19b40914978190b09c2c25874fb868c39e
RAIMI IDRIS <164435300+raimi44@users.noreply.github.com>
2025-11-02 22:57:37 +0100

    Delete bimnar-ict directory

--- bimnar-ict/js/script.js
+++
@@ -1,7 +0,0 @@
-// Toggle mobile menu
-const menuToggle = document.getElementById('menuToggle');
-const navLinks = document.getElementById('navLinks');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
