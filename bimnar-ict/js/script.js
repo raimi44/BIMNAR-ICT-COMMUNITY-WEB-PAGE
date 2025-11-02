6ae76cd1c5f91cdb2c21111cfdf558065ad59b93
RAIMI IDRIS <164435300+raimi44@users.noreply.github.com>
2025-11-02 22:39:03 +0100

    Add files via upload

---
+++ bimnar-ict/js/script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
